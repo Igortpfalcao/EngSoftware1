d7c17610fd16e5cd0dbe8f307be1bf150c3888fe
Igortpfalcao <rogianilorac@gmail.com>
2025-04-03 16:37:17 -0300

    terminado comentario

---
+++ BLOG/Comentario.cs
@@ -0,0 +1,60 @@
+using System;
+
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using nota;
+using usuario;
+
+
+
+namespace comentario{
+    public class Comentario{
+        
+        private string _titulo;
+
+        private string _texto;
+
+        private DateTime _dataDeCriacao;
+
+        private Nota _nota;
+
+        private Usuario _autor;
+
+        public Comentario(string Titulo, string Texto, DateTime DataDeCriacao, Nota Nota, Usuario Autor){
+            _titulo = Titulo;
+
+            _texto = Texto;
+
+            _dataDeCriacao = DataDeCriacao;
+
+            _nota = Nota;
+
+            _autor = Autor;
+        }
+
+        public string Titulo{
+            get => _titulo;
+
+            set => _titulo = value;
+        }
+
+        public string Texto{
+            get => _texto;
+
+            set => _texto = Texto;
+        }
+
+        public DateTime DataDeCriacao{
+            get => _dataDeCriacao;
+        }
+
+        public Nota Nota{
+            get => _nota;
+        }
+        
+        public string Autor{
+            get => _autor.Nome;
+        }
+    }   
+}
