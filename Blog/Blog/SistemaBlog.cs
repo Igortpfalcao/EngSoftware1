16ab72770e38d49b3a8b92b8a7e74b99eed887c9
IgorTPfalcao <rogianilorac@gmail.com>
2025-04-10 11:55:08 -0300

    minor changes

--- Blog/Blog/SistemaBlog.cs
+++ Blog/Blog/SistemaBlog.cs
@@ -7,5 +7,27 @@ namespace sistemaBlog{
     public class SistemaBlog{
         private List<Blog> blogs = new List<Blog>();
         private List <Usuario> usuarios = new List<Usuario>();
+    
+
+   public void criarBlog(String titulo, String emailDono) {
+
+        try{
+		    Usuario dono = buscarUsuarioPorEmail(emailDono);
+            Blog novoBlog = new Blog(titulo, dono, DateTime.Now);
+		    blogs.Add(novoBlog);
+        }
+        catch(InvalidOperationException e){
+            Console.WriteLine("Erro: " + e.Message);
+            
+        }
+	}
+	
+	public Usuario buscarUsuarioPorEmail(String email) {
+		foreach (Usuario usuario in usuarios) {
+			if(email == usuario.Email) 
+				return usuario;
+		}
+		throw new InvalidOperationException("Usuário não encontrado.");
+	}
     }
 }
